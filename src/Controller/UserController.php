63abe26a2757b1a37c05b3a9676a3e25013058f5
Youness Oulamine <younessolm@gmail.com>
2021-01-25 17:44:29 +0100

    traitement d'ajout d'aun annonce

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -26,10 +26,35 @@ class UserController extends AbstractController
      */
     public function ajouteAnnonce(Request $request): Response
     {
-
+        //on crée une instance d'annonce
         $annonce =new Annonces;
-
+        //on créer un forma de type annonce et on le donne notre annonce
         $form = $this->createForm(AnnoncesType::class, $annonce);
+
+        //on donne à notre form la réqueste vient s'il existe
+        $form->handleRequest($request);
+
+        //on teste si notre forme est dêje sbmit est ses champs sont tous valide
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            //si oui on va stocker les informations de nouveau annonce à la base de données
+            
+            //on mettra l'utilisateur actuel comme la créateur de l'annonce 
+            
+            $annonce->setUser($this->getUser());
+            //on stocke le nome de manager de la service de registrer de doctrine 
+            $em = $this->getDoctrine()->getManager();
+
+            //on stocke l'instance d'annonce dans la BD en utilisant le Manager
+            $em->persist($annonce);
+            
+            //On Synchronne l'etat de l'objet avec la base de données
+            $em->flush();
+
+            return $this->redirectToRoute("user_home");
+        }
+
+        
 
         return $this->render('user/annonce/ajoute.html.twig', [
             'form' => $form->createView()
